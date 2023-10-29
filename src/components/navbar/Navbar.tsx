4ccbeed8be3b2b6086c192c493545590f7d7e49a
Mario Caballero <mario.caballero.948@gmail.com>
2023-10-29 18:13:26 -0300

    feat: add navbar styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,19 +4,30 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav>
-      <Image alt="logo" src={""} width={50} height={50} />
-      <Link href="/">¿Quiénes somos?</Link>
-      <Link href="/">Buscar empleo</Link>
-      <Link href="/">Publica tu aviso</Link>
-      <Link href="/">Iniciar sesión</Link>
-      <select>
-        <option value="">Registrate</option>
-        <option value="">Postulante</option>
-        <option value="">Freelance</option>
-        <option value="">Empresa</option>
-        <option value="">ONG</option>
-      </select>
+    <nav className="flex h-28 w-screen justify-evenly bg-white shadow-md shadow-gray-400">
+      <Image
+        alt="logo"
+        src={""}
+        width={50}
+        height={50}
+        className="w-24 border"
+      />
+      <div className="w-2/4 border">
+        <Link href="/">¿Quiénes somos?</Link>
+        <Link href="/">Buscar empleo</Link>
+        <Link href="/">Publica tu aviso</Link>
+      </div>
+      <div className="w-1/3 border">
+        <Link href="/">Iniciar sesión</Link>
+        <select>
+          <option value="">Registrate</option>
+          <option value="">Postulante</option>
+          <option value="">Freelance</option>
+          <option value="">Empresa</option>
+          <option value="">ONG</option>
+        </select>
+        <p>ico</p>
+      </div>
     </nav>
   );
 }
