be5cc26b2e32327ba46c59647f1970bd9d186d25
Mario Caballero <mario.caballero.948@gmail.com>
2023-11-13 11:17:31 -0300

    feat: style on opinions section

--- src/app/components/opinions/components/OpinionCard.tsx
+++ src/app/components/opinions/components/OpinionCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import defaultLogo from "../../../../../public/Avatar.png";
 
 function OpinionCard({
   name,
@@ -13,13 +14,23 @@ function OpinionCard({
   position: string;
 }) {
   return (
-    <div>
-      <header>
-        <Image src={image} alt={name} width={50} height={50} />
-        <h3>{name}</h3>
-        <h4>{position}</h4>
+    <div className="flex h-80 w-96 flex-none flex-col items-center justify-center gap-12 border border-gray-300 bg-white pl-20">
+      <header className="flex w-full items-center gap-6">
+        <Image
+          src={image ? image : defaultLogo}
+          alt={name}
+          width={65}
+          height={65}
+          className="rounded-full"
+        />
+        <div>
+          <h3 className="text-sm capitalize">{name}</h3>
+          <h4 className="mt-1 pl-1 text-xs capitalize text-gray-500">
+            {position}
+          </h4>
+        </div>
       </header>
-      <p>{opinion}</p>
+      <p className="pr-10 text-xs leading-5 text-gray-500">{opinion}</p>
     </div>
   );
 }
