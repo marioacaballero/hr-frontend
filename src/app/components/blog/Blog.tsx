f423b3eb0d81b684ed46f723fc5fc9963ed4a20d
Mario Caballero <mario.caballero.948@gmail.com>
2023-11-24 16:04:24 -0300

    feat: styles on blog

--- src/app/components/blog/Blog.tsx
+++ src/app/components/blog/Blog.tsx
@@ -1,22 +1,37 @@
 import React from "react";
 import BlogCard from "./components/BlogCard";
 
+const blogsHardCode = [
+  {
+    id: "1",
+    title: "title",
+    metadata: "metadata",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim veniam.",
+  },
+  {
+    id: "2",
+    title: "title",
+    metadata: "metadata",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim veniam.",
+  },
+];
+
 function Blog() {
   return (
-    <div>
-      <h2>Nuestro Blog</h2>
-      <BlogCard
-        key={"blog11"}
-        title={"titulo"}
-        metadata={"metadata"}
-        body={"body text"}
-      />
-      <BlogCard
-        key={"blog11"}
-        title={"titulo"}
-        metadata={"metadata"}
-        body={"body text"}
-      />
+    <div className="mb-10 mt-14 flex flex-col items-center justify-between">
+      <h2 className="mb-6 w-full text-center text-3xl font-semibold">
+        Nuestro Blog
+      </h2>
+      <section className="align-center flex w-full justify-center gap-20">
+        {blogsHardCode.map((blog) => (
+          <BlogCard
+            key={blog.id}
+            title={blog.title}
+            metadata={blog.metadata}
+            body={blog.body}
+          />
+        ))}
+      </section>
     </div>
   );
 }
--- src/app/components/blog/components/BlogCard.tsx
+++ src/app/components/blog/components/BlogCard.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import React from "react";
 import { GiShare } from "react-icons/gi";
+import blogD from "../../../../../public/blog.jpg";
 
 function BlogCard({
   title,
@@ -11,14 +13,21 @@ function BlogCard({
   body: string;
 }) {
   return (
-    <div>
-      <header>
-        <h2>{title}</h2>
-        <h4>{metadata}</h4>
+    <div className="flex w-1/3 flex-col border border-gray-300">
+      <header className="">
+        <Image src={blogD} alt="default" className="mb-4 h-28" />
+        <h2 className="indent-5 text-lg capitalize">{title}</h2>
+        <h4 className="indent-5 text-sm capitalize">{metadata}</h4>
       </header>
-      <p>{body}</p>
-      <GiShare />
-      <button>Ver más</button>
+      <p className="p-5">{body}</p>
+      <div className="flex items-center justify-end gap-3 p-6">
+        <div className="text-2xl text-gray-500">
+          <GiShare />
+        </div>
+        <button className="rounded-md bg-gray-300 px-6 py-1 text-xs">
+          Ver más
+        </button>
+      </div>
     </div>
   );
 }
