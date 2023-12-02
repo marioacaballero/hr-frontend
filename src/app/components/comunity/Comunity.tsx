81ec6321eb7a3956b40503e5af131f73899ea1a5
Mario Caballero <mario.caballero.948@gmail.com>
2023-12-02 19:06:46 -0300

    fix: change to full width image

--- src/app/components/comunity/Comunity.tsx
+++ src/app/components/comunity/Comunity.tsx
@@ -8,7 +8,7 @@ function Comunity() {
       <h2 className="mb-6 w-full text-center text-3xl font-medium">
         Nuestra Comunidad
       </h2>
-      <Image src={comunity} alt="comunity" sizes="100vw" />
+      <Image src={comunity} alt="comunity" sizes="100vw" className="w-full" />
     </div>
   );
 }
