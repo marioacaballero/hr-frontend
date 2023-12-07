76237cb2510f8c9e9d60037e085535f05ac94860
Mario Caballero <mario.caballero.948@gmail.com>
2023-12-07 20:08:03 -0300

    feat: add first styles on about page

--- src/app/about/components/purpose/Purpose.tsx
+++ src/app/about/components/purpose/Purpose.tsx
@@ -3,30 +3,37 @@ import React from "react";
 function Purpose() {
   return (
     <div>
-      <h1>Nuestro Propósito</h1>
-      <h3>Nuestra Visión</h3>
-      <p>
-        Ser el espacio donde las oportunidades son para todos, buscamos ayudar a
-        construir un mundo donde la diversidad es valorada y donde todas las
-        personas puedan participar plenamente. Nuestro mayor deseo es ser la red
-        de conexión laboral número uno donde las personas puedan conectarse de
-        manera auténtica y significativa sin importar sus diferencias.
-      </p>
-      <h3>Nuestra Misión</h3>
-      <p>
-        Nuestra misión es hacer el proceso de reclutamiento y selección un
-        proceso humano, fomentando un espacio donde todas las personas puedan
-        sentirse vistas y respetadas, y donde puedan conectarse con su empleo y
-        un otro de manera significativa.
-        <br>
+      <h1 className="p-32 text-center text-4xl font-semibold">
+        Nuestro Propósito
+      </h1>
+      <section className="bg-white px-28 pt-44">
+        <h3 className="border-l-2 border-black pb-10 pl-4 pt-2 text-3xl font-medium">
+          Nuestra Visión
+        </h3>
+        <p className="ml-14 rounded-xl border border-gray-300 p-6 text-xl shadow-md shadow-gray-400">
+          Ser el espacio donde las oportunidades son para todos, buscamos ayudar
+          a construir un mundo donde la diversidad es valorada y donde todas las
+          personas puedan participar plenamente. Nuestro mayor deseo es ser la
+          red de conexión laboral número uno donde las personas puedan
+          conectarse de manera auténtica y significativa sin importar sus
+          diferencias.
+        </p>
+        <h3>Nuestra Misión</h3>
+        <p>
+          Nuestra misión es hacer el proceso de reclutamiento y selección un
+          proceso humano, fomentando un espacio donde todas las personas puedan
+          sentirse vistas y respetadas, y donde puedan conectarse con su empleo
+          y un otro de manera significativa.
+        </p>
+        <p>
           Queremos promover la empatía y la compresión, y trabajar juntos para
           crear un mundo más inclusivo, para ello buscamos ofrecer a las
           compañias el talento humano que necesitan para transformar sus
           culturas a través de un mapa de talento diverso y de esta manera
           construir juntos un mundo más inclusivo y equitativo donde cada
           persona pueda aportar su valor.
-        </br>
-      </p>
+        </p>
+      </section>
     </div>
   );
 }
