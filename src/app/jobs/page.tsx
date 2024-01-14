d62d7e148b5a24f4dc670624e0373aa748e5d4b3
Mario Caballero <mario.caballero.948@gmail.com>
2024-01-14 15:13:00 -0300

    feat: start with new job post

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,34 +1,12 @@
 import React from "react";
+import JobFilters from "./components/JobFilters";
+import JobOffers from "./components/JobOffers";
 
 function Jobs(): React.ReactElement {
   return (
-    <div>
-      <section>
-        <input type="date" />
-        <select>
-          <option value="">modalidad</option>
-          <option value="">presencial</option>
-          <option value="">remoto</option>
-          <option value="">hibrido</option>
-        </select>
-        <select>
-          <option value="">jerarquia</option>
-          <option value="">junior</option>
-          <option value="">semi senior</option>
-          <option value="">senior</option>
-        </select>
-        <select>
-          <option value="">tipo de contratacion</option>
-          <option value="">indeterminado</option>
-          <option value="">temporal</option>
-        </select>
-        <select>
-          <option value="">carga horaria</option>
-          <option value="">full time</option>
-          <option value="">part time</option>
-        </select>
-      </section>
-      <section></section>
+    <div className="flex min-h-screen gap-5 bg-white">
+      <JobFilters />
+      <JobOffers />
     </div>
   );
 }
---
+++ src/app/jobs/post/page.tsx
@@ -0,0 +1,5 @@
+import PostForm from "./components/post-form";
+
+export default function Post() {
+  return <PostForm />;
+}
