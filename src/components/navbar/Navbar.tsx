8709da677e313e9d46e3c50119386d24b9fb1e47
Mario Caballero <mario.caballero.948@gmail.com>
2024-01-18 21:59:21 -0300

    fix: navbar styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -16,44 +16,44 @@ function Navbar() {
   };
 
   return (
-    <nav className="flex h-32 w-screen items-center justify-between border-b border-gray-400 bg-white shadow-sm shadow-gray-400">
+    <nav className="flex h-32 items-center justify-between border-b border-gray-400 bg-white shadow-sm shadow-gray-400">
       <Image
         alt="logo"
         src={logo}
         width={150}
-        className="ml-12 h-24 hover:cursor-pointer"
+        className="ml-4 h-24 hover:cursor-pointer"
         onClick={() => router.push("/")}
       />
       <div className="flex w-2/4 items-center justify-start gap-5 pl-2">
         <Link
           href="/about"
-          className="w-48 rounded-lg border border-verde-loro p-2 text-center duration-500 hover:bg-verde-bg hover:text-verde-loro"
+          className="w-40 rounded-lg border border-verde-loro p-2 text-center duration-500 hover:bg-verde-bg hover:text-verde-loro"
         >
           ¿Quiénes somos?
         </Link>
         <Link
           href="/jobs"
-          className="w-48 rounded-lg border border-verde-loro p-2 text-center duration-500 hover:bg-verde-bg hover:text-verde-loro"
+          className="w-40 rounded-lg border border-verde-loro p-2 text-center duration-500 hover:bg-verde-bg hover:text-verde-loro"
         >
           Buscar empleo
         </Link>
         <Link
           href="/"
-          className="w-48 rounded-lg border border-verde-loro p-2 text-center duration-500 hover:bg-verde-bg hover:text-verde-loro"
+          className="w-40 rounded-lg border border-verde-loro p-2 text-center duration-500 hover:bg-verde-bg hover:text-verde-loro"
         >
           Publica tu aviso
         </Link>
       </div>
-      <div className="flex w-2/6 items-center justify-start gap-6">
+      <div className="flex w-2/6 items-center justify-end gap-6 pr-4">
         <Link
           href="/signin"
-          className="w-36 rounded-lg border border-verde-loro p-3 text-center text-verde-loro duration-500 hover:bg-verde-bg"
+          className="w-36 rounded-lg border border-verde-loro p-2 text-center text-verde-loro duration-500 hover:bg-verde-bg"
         >
           Iniciar sesión
         </Link>
         <select
           onChange={(e) => onChange(e)}
-          className="w-44 rounded-lg bg-verde-loro p-3 text-center text-azul-text"
+          className="w-40 rounded-lg bg-verde-loro p-2 text-center text-azul-text"
         >
           <option value="">Registrate</option>
           <option value="user">Postulante</option>
