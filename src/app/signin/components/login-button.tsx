5cbe0eb9b74b1bf3ebf7838ffba4f277dd2b5a78
Mario Caballero <mario.caballero.948@gmail.com>
2024-02-26 00:47:37 -0300

    feat: add native spiner with tailwind

--- src/app/signin/components/login-button.tsx
+++ src/app/signin/components/login-button.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { ring } from "ldrs";
 import { useFormStatus } from "react-dom";
-
-ring.register();
 
 export function LoginButton() {
   const { pending } = useFormStatus();
@@ -17,17 +14,16 @@ export function LoginButton() {
       >
         Iniciar sesión
       </button>
-      {/* {pending && (
-        <div>
-          <l-ring
-            size="40"
-            stroke="5"
-            bg-opacity="0"
-            speed="2"
-            color="gray"
-          ></l-ring>
+      {pending && (
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-gray-500 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
         </div>
-      )} */}
+      )}
     </div>
   );
 }
--- src/app/signup/components/submit-button.tsx
+++ src/app/signup/components/submit-button.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { ring } from "ldrs";
 import { useFormStatus } from "react-dom";
 import { cuilValidationDb } from "../utils/validations";
-
-ring.register();
 
 export function SubmitButton({
   type,
@@ -26,14 +23,13 @@ export function SubmitButton({
         {displayText}
       </button>
       {pending && (
-        <div className="mt-4">
-          <l-ring
-            size="40"
-            stroke="5"
-            bg-opacity="0"
-            speed="2"
-            color="gray"
-          ></l-ring>
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-gray-500 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
         </div>
       )}
     </>
